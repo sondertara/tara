759161bc2fd634d77931fcda3e39ad749cb20b6a
huangxiaohu <huangxiaohu.chn@outlook.com>
2023-01-18 15:07:21 +0800

    feat: Stand JDK8

--- notify-tara/src/main/java9/module-info.java
+++
@@ -1,7 +0,0 @@
-module com.sondertara.notify {
-    requires lombok;
-    requires com.sondertara.common;
-    requires com.alibaba.fastjson2;
-    requires java.mail;
-    requires unirest.java;
-}
